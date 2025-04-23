2fb8054db9f42f0d84bf8f4f9e962675ca32cd53
Dom <dom@tide.org>
2025-04-23 17:37:21 +1000

    just update path for imports.

--- lib/tidecloak.ts
+++ lib/tidecloak.ts
@@ -1,6 +1,6 @@
-// Use CommonJS require to load heimdall's exports
-import heimdall = require("@/external/tidecloak-js/lib/heimdall");
+import heimdall = require("../../tidecloak-js/lib/heimdall");
 
 export const base64ToBytes = heimdall.base64ToBytes;
 export const bytesToBase64 = heimdall.bytesToBase64;
 export const getHumanReadableObject = heimdall.getHumanReadableObject;
+
